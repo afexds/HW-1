9e6422c94016509e9b4b38eccf15420415c1f514
afex <112443035+afexds@users.noreply.github.com>
2022-09-13 13:05:44 -0400

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,15 @@
+num = 100
+
+total = sum(range(1, num + 1))
+print(total)  
+
+# [1, 2, 3, ... 98, 99, 100]
+print(list(range(1, num + 1)))
+
+
+
+#  alternatively, you can use a formula
+
+total_2 = num * (num + 1) // 2
+
+print(total_2)  #5050
